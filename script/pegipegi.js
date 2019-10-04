a0cc517c0c5e2891cb03926b1bb454b73105b2b5
andreassosilo <andreas.sosilo@gmail.com>
2019-10-04 13:28:30 +0700

    Add flight integration to the client

--- script/pegipegi.js
+++ script/pegipegi.js
@@ -2,6 +2,13 @@ function pegipegi() {
   let from = $('#from').val()
   let to = $('#to').val()
   let date = $('#date').val()
+
+  Swal.fire({
+    title: `Fetching data from server...`,
+    allowOutsideClick: () => !Swal.isLoading()
+  });
+  Swal.showLoading();
+
   $.ajax({
     method: 'post',
     url: `${baseURL}/pegipegi`,
@@ -12,8 +19,12 @@ function pegipegi() {
     }
   })
     .done((response) => {
+      $('.weather').empty()
+      $('#price-box').empty()
       console.log(response)
       weather(response.weather.hours[0], response.flight.Places[1].CityName)
+      flight(response.flight)
+      Swal.close()
     })
     .fail((err) => {
       console.log(err)
@@ -33,4 +44,35 @@ function weather(data, city) {
     </div>
   </div>
  `)
+}
+
+function flight(data) {
+  if (data.Quotes.length === 0) {
+    $('#price-box').append(`
+      <h1>No data is found for that specific date</h1>
+    `)
+  } else {
+    let date = new Date(data.Quotes[0].OutboundLeg.DepartureDate).toLocaleDateString()
+    $('#price-box').append(`
+    <div class="card m-3">
+      <img id="ticket-img"
+        src="https://static.makeuseof.com/wp-content/uploads/2013/11/tickets-money-670x335.jpg"
+        class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">Lowest Fare Ticket</h5>
+        <div id="head">
+            <p class="card-text"><i class="fas fa-building"></i> ${data.Carriers[0].Name}</p>
+        </div>
+        <div id="center">
+            <p class="card-text"><i class="fas fa-plane-departure"></i> ${data.Places[0].CityName}</p>
+            <p class="card-text"><i class="fas fa-plane-arrival"></i> ${data.Places[1].CityName}</p>
+        </div>
+        <div id="bottom">
+            <p class="card-text"><i class="fas fa-calendar-day"></i> ${date}</p>
+            <p class="card-text"><i class="fas fa-coins"></i> Rp ${data.Quotes[0].MinPrice},-</p>
+        </div>
+      </div>
+    </div>
+  `)
+  }
 }
